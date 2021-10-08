5cb56d3d1feef6954d177aa14721f29c186db3c2
morawia <janmora60@gmail.com>
2021-10-08 19:58:47 +0200

    Datentypen initialisieren

--- SRC/Calculator.java
+++ SRC/Calculator.java
@@ -1,6 +1,20 @@
 public class Calculator {
 
     public static void main(String[] args) {
+        double numberKomma = 2.333;
+        System.out.println(numberKomma);
+
+        boolean Wahrheit = false;
+        System.out.println(Wahrheit);
+
+        char Zeichen = 'c';
+        System.out.println(Zeichen);
+
+        String Zeichenkette = "DIMI ist echt korrekt";
+        System.out.println(Zeichenkette);
+
+        int number = 1;
+        System.out.println(number);
 
     }
 }
