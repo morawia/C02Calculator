a1818076231c81073644d6cc4ab92d3d69ecc575
morawia <janmora60@gmail.com>
2021-10-08 20:26:04 +0200

    arithmetische Operationen

--- SRC/Calculator.java
+++ SRC/Calculator.java
@@ -16,5 +16,17 @@ public class Calculator {
         int number = 1;
         System.out.println(number);
 
+        int value1 = 4;
+        int value2 = 3;
+        //addition
+        System.out.println("addition: " + (value1+value2));
+        //subtraction
+        System.out.println("subtraction: "+ (value1 - value2));
+        //multiplikation
+        System.out.println("multiplication: "+ value1*value2);
+        //division
+        System.out.println("division: "+ value1/value2);
+        //modulo
+        System.out.println("Modulo: "+ value1%value2);
     }
 }
